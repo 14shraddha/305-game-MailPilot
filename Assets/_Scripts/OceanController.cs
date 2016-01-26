da53990e5dd86c739ac51fc4ba8179ff357064bc
14shraddha <you@example.com>
2016-01-26 11:58:05 -0500

    first commit

---
+++ Assets/_Scripts/OceanController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OceanController : MonoBehaviour {
+
+    private Transform _transform;
+    private Vector2 _currentPosition;
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //make refrence to transform componnt in unity
+        this._transform = gameObject.GetComponent<Transform>();
+
+        // this._transform.position = new Vector2(0, 480f); // to create the position and always use vector to assign 
+
+        this.Reset();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(0, this.speed);
+        this._transform.position = this._currentPosition;
+
+        if (this._currentPosition.y <= -480)
+        {
+            this.Reset();
+        }
+	}
+
+    public void Reset()
+    {
+        this._transform.position = new Vector2(0, 480f);
+    }
+}
---
+++ Assets/_Scripts/PlaneController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+   
+    //Private
+    private float _playerInput;
+    private Transform _transform;
+    private Vector2 _currentPosition;
+
+    //public
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //small gameobject refers to the componennt attached to
+        this._transform = gameObject.GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._currentPosition = this._transform.position;
+
+        this._playerInput = Input.GetAxis("Horizontal");
+        //Positive movement move right
+        if (this._playerInput > 0)
+        {
+            this._currentPosition += new Vector2(this.speed, 0);
+
+        }
+        //Positive movement move right
+        if (this._playerInput < 0)
+        {
+            this._currentPosition -= new Vector2(this.speed, 0);
+        }
+        this._checkBounds();
+    }
+    //***************PRIVATE METHODS******************************************************************
+    private void _checkBounds()
+    {
+        //checked if the plane goes out of boundary and keep it inside
+        if (this._currentPosition.x < -290)
+        {
+            this._currentPosition.x = -290;
+        }
+        if (this._currentPosition.x > 290)
+        {
+            this._currentPosition.x = 290;
+        }
+
+        this._transform.position = this._currentPosition;
+    }
+
+    
+}
